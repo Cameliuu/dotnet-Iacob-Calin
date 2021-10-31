d470ef1c6b04c4b5a1676f953f61d90c4ade951b
Iacob Calin Ionut <calin.iacob@student.upt.ro>
2021-10-31 19:49:35 +0200

    Reading user input, passing variables by reference, reading data types other than strings

--- dotnet-challenge/Program.cs
+++ dotnet-challenge/Program.cs
@@ -7,14 +7,26 @@ namespace Iacob_Calin
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.print();
+            string nume="%0";
+            double nota1=0, nota2=0, nota3=0;
+            program.input(ref nume, ref nota1, ref nota2, ref nota3);
+            program.medie(nume, nota1, nota2, nota3);
         }
-        void print()
+        void medie(string n, double a, double b, double c)
         {
-         
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Printing stuff using methods");
+           double  medie = (a + b + c) / 3;
+            Console.WriteLine($"{n}, nota dumneavoastra la examenul de bacalaureat este {medie}");
+        }
+        void input(ref string n, ref double a, ref double b, ref double c )
+        {
+            Console.WriteLine("Introduceti numele dumneavoastra: ");
+            n = Console.ReadLine();
+            Console.WriteLine("Introduceti nota primita la prima proba: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduceti nota primita la cea de a doua proba: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduceti nota primita la cea de a treia proba: ");
+            b = Convert.ToDouble(Console.ReadLine());
 
         }
     }
