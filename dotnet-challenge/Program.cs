8f122fd67372b33e04b3a5b3e88bdb07095926b3
Iacob Calin Ionut <calin.iacob@student.upt.ro>
2021-10-31 18:56:42 +0200

    Static methods, Instance methods

---
+++ dotnet-challenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Iacob_Calin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.print();
+        }
+        void print()
+        {
+         
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Printing stuff using methods");
+
+        }
+    }
+}
